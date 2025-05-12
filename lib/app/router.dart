8d92eb4579b9f0f705e91720e9b8939cb77be253
gulistansenates <gulistansenates@gmail.com>
2025-05-13 00:43:43 +0300

    feat : Add AppBar and BottomNavigationBar for main navigation layout

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,13 +1,65 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rick_and_morty_app/views/app_view.dart';
+import 'package:rick_and_morty_app/views/characters/characters_view.dart';
+import 'package:rick_and_morty_app/views/favorites/favorites_view.dart';
+import 'package:rick_and_morty_app/views/locations/locations_view.dart';
+import 'package:rick_and_morty_app/views/sections/sections_view.dart';
+
+final _routerKey = GlobalKey<NavigatorState>();
+
+class AppRoutes {
+  AppRoutes._();
+
+  static const String characters = '/';
+  static const String favorites = '/favorites';
+  static const String locations = '/locations';
+  static const String sections = '/sections';
+  static const String settings = '/settings';
+}
 
 final router = GoRouter(
+  navigatorKey: _routerKey,
+  initialLocation: AppRoutes.characters,
   routes: [
     StatefulShellRoute.indexedStack(
-      builder:
-          (context, state, navigationShell) =>
-              AppView(navigationShell: navigationShell),
-      branches: const [],
+      builder: (context, state, navigationShell) {
+        return AppView(navigationShell: navigationShell);
+      },
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoutes.characters,
+              builder: (context, state) => const CharactersView(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoutes.favorites,
+              builder: (context, state) => const FavoritesView(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoutes.locations,
+              builder: (context, state) => const LocationsView(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: AppRoutes.sections,
+              builder: (context, state) => const SectionsView(),
+            ),
+          ],
+        ),
+      ],
     ),
   ],
 );
