59e46719272403c3be03843635ff26602c9eade5
gulistansenates <gulistansenates@gmail.com>
2025-05-15 22:36:46 +0300

    Integrate global theme management with Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rick_and_morty_app/app/locator.dart';
 import 'package:rick_and_morty_app/app/router.dart';
 import 'package:rick_and_morty_app/app/theme.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupLocator();
+  runApp(
+    MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => AppTheme())],
+      child: const MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
+    return Consumer<AppTheme>(
+      builder:
+          (context, viewModel, child) => MaterialApp.router(
+            routerConfig: router,
+            debugShowCheckedModeBanner: false,
+            theme: viewModel.theme,
+          ),
     );
   }
 }
